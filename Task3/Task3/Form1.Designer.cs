a30f956ae9d470adbbc2fb79939e4b88373039b1
unknown <a_krekesova@c441b-10.kbtu.kz>
2017-05-20 10:11:28 +0600

    10 10

--- Task3/Task3/Form1.Designer.cs
+++ Task3/Task3/Form1.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "tick";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(111, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(132, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(83, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 153);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(35, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(19, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(53, 77);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label12.Location = new System.Drawing.Point(77, 55);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- Task3/Task3/Form1.cs
+++ Task3/Task3/Form1.cs
@@ -16,5 +16,358 @@ namespace Task3
         {
             InitializeComponent();
         }
+       
+        int cnt = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            cnt %= 12;
+            if (cnt == 1)
+            {
+                label1.ForeColor = System.Drawing.Color.Red;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+            }
+            if (cnt == 2)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.Color.Red;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+            }
+            if (cnt == 3)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.Color.Red;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+            
+            }
+
+            if (cnt == 4)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.Color.Red;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 5)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.Color.Red;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 6)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.Color.Red;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 7)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.Color.Red;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 8)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.Color.Red;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 9)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.Color.Red;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 10)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.Color.Red;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 11)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.Color.Red; 
+                label12.ForeColor = System.Drawing.SystemColors.ControlText;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+            if (cnt == 0)
+            {
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+                label3.ForeColor = System.Drawing.SystemColors.ControlText;
+                label4.ForeColor = System.Drawing.SystemColors.ControlText;
+                label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                label10.ForeColor = System.Drawing.SystemColors.ControlText;
+                label11.ForeColor = System.Drawing.SystemColors.ControlText;
+                label12.ForeColor = System.Drawing.Color.Red; ;
+                label1.Text = "1";
+                label2.Text = "2";
+                label3.Text = "3";
+                label4.Text = "4";
+                label5.Text = "5";
+                label6.Text = "6";
+                label7.Text = "7";
+                label8.Text = "8";
+                label9.Text = "9";
+                label10.Text = "10";
+                label11.Text = "11";
+                label12.Text = "12";
+
+            }
+        }
     }
 }
