d30830488f0ea62da5c65b4cbf7c9861429abaea
unknown <a_krekesova@c441b-10.kbtu.kz>
2017-05-20 10:31:19 +0600

    10 30

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -11,7 +11,40 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            DirectoryInfo papka = new DirectoryInfo(@"C:\Users\a_krekesova\Desktop\Task1");
+            FileInfo[] files = papka.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                Console.Write(file.FullName);
+                StreamReader sr = new StreamReader(file.FullName+" ");
+                string s = sr.ReadToEnd();
+                char b=' ';
+                string ss="";
+                foreach (char c in s)
+                {
+                    if (b == ' ') {
+                        int n = int.Parse(ss);
+                        int m = 1;
+                        for (int i = 1; i <= 15; i++)
+                        {
+                            if (m == n)
+                            {
+                                Console.Write(m.ToString()+ " ");
+                            }
+                            m *= 2;
+                        }
+                            ss = ""; 
+                    }
+                    else
+                    {
+                        if (c >= '0' && c <= '9')
+                        {
+                            ss += c;
+                        }
+                    }
+                    b = c;
+                }
+            }
         }
     }
 }
